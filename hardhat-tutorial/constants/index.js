8a4f21eefb18cf262c2ef36692a2b7975cc42c66
Aspera901 <vodico1@gmail.com>
2022-02-05 14:51:03 -0500

    commit2

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x708C0B8fA1af7172f8b48422c6F12efCD1B4E03F";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
